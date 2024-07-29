182a0cddf231b6c0c591e5e546d17301546dbe9c
aavadhan10 <166965425+aavadhan10@users.noreply.github.com>
2024-07-29 16:30:07 -0500

    Update rolodex_v2_main.py

--- rolodex_v2_main.py
+++ rolodex_v2_main.py
@@ -87,11 +87,12 @@ def query_gpt_with_data(question, matters_data, matters_index, matters_vectorize
         }
 
         # Display the results without the index
-        st.write("Top Recommended Lawyer Based on Filtered Data:")
-        st.write(pd.DataFrame([top_recommended_lawyer_details]).to_html(index=False), unsafe_allow_html=True)
-        st.write("Other Recommended Lawyers:")
+        
+        st.write("Top Recommended Lawyers across Practice Area:")
         st.write(recommendations_df.to_html(index=False), unsafe_allow_html=True)
-        st.write("All Filtered Lawyers:")
+        st.write("Top Recommended Lawyer Based on Specific Need & Data:")
+        st.write(pd.DataFrame([top_recommended_lawyer_details]).to_html(index=False), unsafe_allow_html=True)
+        st.write("Reasoning Behind Recommendation :")
         st.write(filtered_data.to_html(index=False), unsafe_allow_html=True)
     except Exception as e:
         st.error(f"Error querying GPT: {e}")
